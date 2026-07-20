b3a29ead1cdeee64250245e2dcbf2b70563ba20d
Zixi Qi <qizixi2@126.com>
2026-07-21 04:39:04 +1000

    ci: gitea workflow new fix

--- scripts/pre-install.sh
+++ scripts/pre-install.sh
@@ -1,47 +1,21 @@
-name: Build and cross-compile
+#!/bin/bash
+# set -e
 
-on:
-  push:
-  pull_request:
-  workflow_dispatch:
+# 1. 切换为 32位 armhf 架构
+dpkg --add-architecture armhf
+echo "running apt update"
+apt-get update
+echo "installing dependencies"
 
-jobs:
-  build-firmware:
-    runs-on: ubuntu-24.04
-    steps:
-      - name: Check out repository code
-        uses: actions/checkout@v4
-
-      - name: Install Rust toolchain
-        uses: dtolnay/rust-toolchain@stable
-        with:
-          targets: armv7-unknown-linux-gnueabihf
-
-      - name: Install ARMv7 Cross-Compiler and Dependencies
-        run: |
-          sudo dpkg --add-architecture armhf
-          sudo apt-get update
-
-          sudo apt-get install -y gcc-arm-linux-gnueabihf pkg-config
-
-          sudo apt-get install -y \
-              libgstreamer1.0-dev:armhf \
-              libgstreamer-plugins-base1.0-dev:armhf \
-              libgstreamer-plugins-good1.0-dev:armhf \
-              libgstrtspserver-1.0-dev:armhf \
-              gstreamer1.0-plugins-base:armhf \
-              gstreamer1.0-plugins-good:armhf \
-              gstreamer1.0-plugins-bad:armhf \
-              gstreamer1.0-plugins-ugly:armhf \
-              gstreamer1.0-tools:armhf \
-              libglib2.0-dev:armhf
-
-      - name: Build for armv7-unknown-linux-gnueabihf
-        run: |
-          export CARGO_TARGET_ARMV7_UNKNOWN_LINUX_GNUEABIHF_LINKER=arm-linux-gnueabihf-gcc
-          
-          export PKG_CONFIG_ALLOW_CROSS=1
-          export PKG_CONFIG_PATH=/usr/lib/arm-linux-gnueabihf/pkgconfig:/usr/share/pkgconfig
-          export PKG_CONFIG_SYSROOT_DIR=/
-          
-          cargo build --release --target armv7-unknown-linux-gnueabihf
+# 2. 安装编译所需的 32位 依赖库（包含 gstreamer 核心、插件以及 RTSP 服务器组件）
+apt-get install -y \
+    libgstreamer1.0-dev:armhf \
+    libgstreamer-plugins-base1.0-dev:armhf \
+    libgstreamer-plugins-good1.0-dev:armhf \
+    libgstrtspserver-1.0-dev:armhf \
+    gstreamer1.0-plugins-base:armhf \
+    gstreamer1.0-plugins-good:armhf \
+    gstreamer1.0-plugins-bad:armhf \
+    gstreamer1.0-plugins-ugly:armhf \
+    gstreamer1.0-tools:armhf \
+    libglib2.0-dev:armhf
